8a458655dbc984aa768b56ef2c7e2316722fd79d
Mark Truitt <matruitt30@gmail.com>
2023-02-20 19:55:51 -0500

    Setup TemperatureConverter class based on specs
     Constructors which call parent constructors
     Overridden convert() method to convert input (Fahrenheit temperature) to Celsius and returns the value. If the instance has no input value, it should return Double.NaN
     Use the following formula for conversion: C = ((F-32)*5)/9

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -1,2 +1,26 @@
-public class TemperatureConverter {
+public class TemperatureConverter extends Converter {
+
+    // Constructor
+    public TemperatureConverter() {
+        super();
+    }
+
+    // Overloaded Constructor
+    public TemperatureConverter(double input) {
+        super(input);
+    }
+
+    /**
+     * Converts the input Fahrenheit temperature to Celsius.
+     * Formula: °C = 5/9(°F – 32) or °C = .5556(°F – 32)
+     * If the instance has no input value, it returns Double.NaN.
+     * @return the converted temperature in Celsius, or Double.NaN if no input value is set
+     */
+    @Override
+    public double convert() {
+        if(Double.isNaN(getInput())) {
+            return Double.NaN;
+        }
+        return ((getInput() - 32) * 5) / 9;
+    }
 }
