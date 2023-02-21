a52a9348814ab141e0d2f736914cf0988d8eb2dc
Mark Truitt <matruitt30@gmail.com>
2023-02-20 19:40:27 -0500

    Setup Converter class based on specs
     Private attribute for input of data type double
     Default constructor with no parameter which sets input to Double.NaN
     Overloaded constructor with input for parameter
     Get and set methods for input attribute
     Method convert() which returns input value

--- src/Converter.java
+++ src/Converter.java
@@ -1,2 +1,34 @@
 public class Converter {
+    // attribute
+    private double input;
+
+    // constructor
+    public Converter() {
+        this.input = Double.NaN;
+    }
+
+    // Overloaded constructor
+    public Converter(double input) {
+        this.input = input;
+    }
+
+    // Getter
+    public double getInput() {
+        return input;
+    }
+
+    // Setter
+    public void setInput(double input) {
+        this.input = input;
+    }
+
+    /**
+     * Convert method
+     * Subclasses will override this method to provide specific conversion functionality.
+     * @return the converted value, or the original input if not converted
+     */
+    public double convert() {
+        return input;
+    }
+
 }
